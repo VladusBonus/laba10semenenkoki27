59d1adc229c9d9b40d3feccaafb8a825e2d3f1b6
Vladus <76747919+VladusBonus@users.noreply.github.com>
2025-05-25 23:10:48 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,34 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+
+
+export default nextConfig;
+
+
